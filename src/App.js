deb7481bd5a1a86cea719c1a8400352f084fcc7c
JulieChoJulie <h32cho@gmail.com>
2019-03-07 17:37:16 -0500

    template done

--- src/App.js
+++ src/App.js
@@ -1,28 +1,13 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, {Component} from 'react';
+import TodoListTemplate from './components/TodoListTemplate';
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <TodoListTemplate>
+                템플릿 완성
+            </TodoListTemplate>
+        );
+    }
 }
 
 export default App;
---
+++ src/components/TodoListTemplate.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './TodoListTemplate.css';
+
+
+// 함수형 컴포넌트
+// parameter : (prop) => { ... }
+const TodoListTemplate = ({form, children}) => {
+    return(
+        <main className="todo-list-template">
+            <div className="title">
+                오늘 할 일
+            </div>
+            <section className="form-wrapper">
+                {form}
+            </section>
+            <section className="todos-wrapper">
+                {children}
+            </section>
+        </main>
+    )
+};
+
+export default TodoListTemplate;
